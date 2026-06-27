ae8cb2dfefcdff2a5aa7c688bfd19ee2d60c13c5
Eman3655 <emmoabdelhamid2021@gmail.com>
2026-06-27 10:19:40 +0300

    update notification

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,14 +1,15 @@
+// public/sw.js
 self.addEventListener("push", (event) => {
   const data = event.data ? event.data.json() : { title: "مخيم الرواد", message: "لديك إشعار جديد" };
 
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.message,
-      icon: "/icons/icon-192x192.png", 
-      badge: "/icons/badge-72x72.png",
+      icon: "/uploads/logo.png", 
+      badge: "/uploads/logo.png", 
       vibrate: [200, 100, 200],
       data: {
-        url: data.url || "/" 
+        url: data.url || "/"
       }
     })
   );
